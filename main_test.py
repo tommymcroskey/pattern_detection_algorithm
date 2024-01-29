f70e2e6a5ef50466b38d754c696af1d42f24b0c3
tommymcroskey <mcroskey.thomas@gmail.com>
2024-01-29 12:22:09 -0700

    added test functions

--- main_test.py
+++ main_test.py
@@ -4,6 +4,18 @@ import main
 
 class Test_Main(unittest.TestCase):
 
+    def test_Empty_Input(self):
+        self.assertEqual(main.repeat(), None)
+    
+    def test_One_Element_Input(self):
+        self.assertEqual(main.repeat([1]), None)
+
+    def test_Empty_List(self):
+        self.assertEqual(main.repeat([]), None)
+    
+    def test_String_Input(self):
+        self.assertEqual(main.repeat("HelloHello"), "Hello")
+
     def test_Large_Pattern(self):
         # Generate a test pattern with 20 random integers
         test_pattern = [random.randint(0, 20) for _ in range(20)]
